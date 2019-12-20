6c29975a586611396ec4038d0e2719f4b833cad6
CallingST3 <thewobblebass@gmail.com>
2019-12-20 14:50:49 +0300

    bottom nav + firebase dependencies

--- app/src/main/java/com/apps/findmyjob/ui/MainActivity.kt
+++ app/src/main/java/com/apps/findmyjob/ui/MainActivity.kt
@@ -1,30 +1,39 @@
 package com.apps.findmyjob.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.apps.findmyjob.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    
+
     private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        navController = Navigation.findNavController(this, R.id.navFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
 
-        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+        navController = findNavController(R.id.navFragment)
+        val navView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
 
-            true
-        }
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.searchFragment, R.id.favoriteFragment, R.id.profileFragment
+        ))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+
 
     }
+
+
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, null)
